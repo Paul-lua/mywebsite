d1ba669317c9b125aa786fc34e5862298d31ac3f
Enzo <96349426+EnzoTuCoco@users.noreply.github.com>
2023-01-02 11:45:13 +0100

    Add files via upload

---
+++ portfolio/static/js/script.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('#container h1', { duration: 1e3,
+delay: 0,
+scale: .5 }),
+ScrollReveal().reveal("#container ul", {
+duration: 1e3,
+delay: 750
+})
+console.log("%cyour%c mom", "font-size:128px;background:transparent;color:#eeeeee;font-family:'Raleway',Helvetica Neue,Helvetica,sans-serif;text-shadow: 1px 1px 4px #111111", "font-size:128px;background:transparent;color:#f97b7b;font-family:'Raleway',Helvetica Neue,Helvetica,sans-serif;text-shadow: 1px 1px 4px #111111", "font-size:32px;color:#f97b7b;font-family:'Raleway';background:transparent;text-shadow: 1px 1px 4px #111111") 
